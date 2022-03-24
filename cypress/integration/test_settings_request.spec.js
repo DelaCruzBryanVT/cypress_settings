a5f91520d856f158f9d3888d2971eb9e85696d49
Dikson Calderon <Dikson.Calderon@connectinformatics.com>
2022-03-24 21:54:00 +0100

    POST Request

--- cypress/integration/test_settings_request.spec.js
+++ cypress/integration/test_settings_request.spec.js
@@ -1,31 +1,100 @@
 import { Guid } from 'guid-typescript';
 
 describe('TEST HTTP REQUEST', () => {
-    let idGuid=Guid.create().value
-    let token="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiY2EiLCJodHRwOi8vd3d3LmVxdWlwZS5jb20vd3MvMjAwOS8wOS9pZGVudGl0eS9jbGFpbXMvaWRTZXNzaW9uIjoiNzIxNGQzMGQtYjQ0NC00ZjY4LTlmYjEtMjZhNGI2OGIwMTY4IiwiaHR0cDovL3d3dy5lcXVpcGUuY29tL3dzLzIwMDkvMDkvaWRlbnRpdHkvY2xhaW1zL3Nlc3Npb25JZCI6IjkxNTU4ODIwODc0OTgyOTA3MjE0RDMwRC1CNDQ0LTRGNjgtOUZCMS0yNkE0QjY4QjAxNjgiLCJodHRwOi8vd3d3LmVxdWlwZS5jb20vd3MvMjAwOS8wOS9pZGVudGl0eS9jbGFpbXMvY2xpZW50QWdlbnRJZCI6IjJlNGE0NTQ4LTJmY2YtNDI5Ny04OWUxLTE0YjdmODRkYWZkMSIsImV4cCI6MTY0ODE1MDE4NSwiaXNzIjoiQ05FVC5FcXVpcGUzLldlYi5XZWJBcGkiLCJhdWQiOiJBdWRpZW5jZSJ9.14MGLYgKeq5OgUVV1iSRQwCS36gq-n3newu4evgFhP8"
-    
+    let idGuid = Guid.create().value
+    let token = "Bearer " +
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiY2EiLCJodHRwOi8vd3d3LmVxdWlwZS5jb20vd3MvMjAwOS8wOS9pZGVudGl0eS9jbGFpbXMvaWRTZXNzaW9uIjoiODExYzdlY2MtMGFmNC00MzUwLWJlZmItMGQ3Y2U1MDg2NzIwIiwiaHR0cDovL3d3dy5lcXVpcGUuY29tL3dzLzIwMDkvMDkvaWRlbnRpdHkvY2xhaW1zL3Nlc3Npb25JZCI6IjI1MzE4MjkyNDczNDE4ODg4MTFDN0VDQy0wQUY0LTQzNTAtQkVGQi0wRDdDRTUwODY3MjAiLCJodHRwOi8vd3d3LmVxdWlwZS5jb20vd3MvMjAwOS8wOS9pZGVudGl0eS9jbGFpbXMvY2xpZW50QWdlbnRJZCI6IjJlNGE0NTQ4LTJmY2YtNDI5Ny04OWUxLTE0YjdmODRkYWZkMSIsImV4cCI6MTY0ODE1NTg4NiwiaXNzIjoiQ05FVC5FcXVpcGUzLldlYi5XZWJBcGkiLCJhdWQiOiJBdWRpZW5jZSJ9.aE9_TRROtr_2w3Kgdgr-S_kYV8eECujBCx0z90n0AdY"
+
     beforeEach(() => {
         cy.login();
         cy.wait(4000);
     })
 
-    it("GET Care Folder Output Template Backup", () => {
-        cy.getRequest('https://debugsite.myequipe2/webapi/api/v1/CareFolderOutputTemplateBackup/GetConfigurationList',token,15,'cypress/fixtures/careFolderOutputTemplateBackup.json');
+    //#region GET REQUEST
+    // it("GET Care Folder Output Template Backup", () => {
+    //     cy.getRequest('https://debugsite.myequipe2/webapi/api/v1/CareFolderOutputTemplateBackup/GetConfigurationList', token, 15, 'cypress/fixtures/careFolderOutputTemplateBackup.json');
+    // });
+
+    // it("GET Associated Contexts", () => {
+    //     cy.getRequest('https://debugsite.myequipe2/webapi/api/v1/CareFolderOutputTemplateBackupHasContext/GetConfigurationList?careFolderOutputTemplateBackupId=e4d23a31-1a6a-786e-f193-004e6d993ab9', token, 3, 'cypress/fixtures/associatedContexts.json');
+    // });
+
+    // it("GET Associated Report Launcher", () => {
+    //     cy.getRequest('https://debugsite.myequipe2/webapi/api/v1/CareFolderOutputTemplateBackupHasReportLauncher/GetConfigurationList?careFolderOutputTemplateBackupId=e4d23a31-1a6a-786e-f193-004e6d993ab9', token, 2, 'cypress/fixtures/associatedLauncher.json');
+    // });
+
+    // it("GET Associated Schedules", () => {
+    //     cy.getRequest('https://debugsite.myequipe2/webapi/api/v1/CareFolderOutputTemplateBackupHasSchedule/GetConfigurationList?careFolderOutputTemplateBackupId=e4d23a31-1a6a-786e-f193-004e6d993ab9', token, 3, 'cypress/fixtures/associatedSchedules.json');
+    // });
+    //#endregion
+
+    //#region POST REQUEST
+    it("POST Care Folder Output Template Backup", () => {
+        let body = {
+            $id: "0",
+            basePath: "TEST CYPRESS 1",
+            careFolderOutputTemplateBackupNotificationTypeId: 1,
+            careFolderOutputTemplateId: "d01d5ce4-71d2-e7fc-b2a5-8de6c0055309",
+            deleteFolderOnFailure: true,
+            endDate: "2022-03-23T23:00:00.000Z",
+            fromDate: "('-7d.PreviousMonday.StartOfDay')",
+            id: "C70001A1-4D36-A4B4-0C68-117C7359E164",
+            includeDischargedHospitalizations: false,
+            isEnabled: false,
+            minimumFreeSpaceInBytes: null,
+            name: "TEST CYPRESS 1",
+            notifyToEmail: null,
+            retentionCount: null,
+            startDate: "2022-03-23T23:00:00.000Z",
+            toDate: "('+0d')"
+        };
+        cy.postRequest('https://debugsite.myequipe2/webapi/api/v1/CareFolderOutputTemplateBackupSettings/Save', token, body, "c70001a1-4d36-a4b4-0c68-117c7359e164",0);
     });
 
-    it("GET Associated Contexts", () => {
-        cy.getRequest('https://debugsite.myequipe2/webapi/api/v1/CareFolderOutputTemplateBackupHasContext/GetConfigurationList?careFolderOutputTemplateBackupId=e4d23a31-1a6a-786e-f193-004e6d993ab9',token,3,'cypress/fixtures/associatedContexts.json');
+    it("POST Associated Contexts", () => {
+        let body = {
+            $id: "1",
+            basePath: "Base Path",
+            careFolderOutputTemplateBackupId: "c70001a1-4d36-a4b4-0c68-117c7359e164",
+            entity: 10088,
+            entityId: 10088,
+            entityInstanceId: "70af513b-a5b1-43d6-8613-f8d872f707db",
+            id: "c2b5b868-d772-7ae4-7aa3-f1bc65809d9a",
+            isEnabled: true,
+        };
+        cy.postRequest('https://debugsite.myequipe2/webapi/api/v1/CareFolderOutputTemplateBackupHasContext/Save', token, body, 'c70001a1-4d36-a4b4-0c68-117c7359e164',1);
     });
 
-    it("GET Associated Report Launcher", () => {
-        cy.getRequest('https://debugsite.myequipe2/webapi/api/v1/CareFolderOutputTemplateBackupHasReportLauncher/GetConfigurationList?careFolderOutputTemplateBackupId=e4d23a31-1a6a-786e-f193-004e6d993ab9',token,2,'cypress/fixtures/associatedLauncher.json');
-   
+    it("POST Associated Report Launcher", () => {
+        let body = {
+            $id: "1",
+                        careFolderOutputTemplateBackupId: "c70001a1-4d36-a4b4-0c68-117c7359e164",
+                        customParameters: "Report Launcher",
+                        filename: "Filename",
+                        id: "385e7095-ac5b-6d1b-16e7-cdf6c1c6a14a",
+                        reportLauncherId: "d3863d79-83cc-4bc1-9311-72ea5bbfeb3e",
+        };
+        cy.postRequest('https://debugsite.myequipe2/webapi/api/v1/CareFolderOutputTemplateBackupHasReportLauncher/Save', token, body,'c70001a1-4d36-a4b4-0c68-117c7359e164',1);
     });
 
-    it("GET Associated Schedules", () => {
-        cy.getRequest('https://debugsite.myequipe2/webapi/api/v1/CareFolderOutputTemplateBackupHasSchedule/GetConfigurationList?careFolderOutputTemplateBackupId=e4d23a31-1a6a-786e-f193-004e6d993ab9',token,3,'cypress/fixtures/associatedSchedules.json');
-
+    it("POST Associated Schedules", () => {
+        let body = {
+            $id: "1",
+            careFolderOutputTemplateBackupId: "c70001a1-4d36-a4b4-0c68-117c7359e164",
+            dayOfMonth: null,
+            dayOfWeek: null,
+            hour: null,
+            id: "5d6b4a99-4d85-7268-f381-262cfc00589a",
+            isEnabled: false,
+            maxDegreeOfParallelism: 1,
+            minute: null,
+            month: null,
+            retryCount: 1,
+            timeoutInSeconds: 1,
+            waitForPreviousToComplete: true,
+        };
+        cy.postRequest('https://debugsite.myequipe2/webapi/api/v1/CareFolderOutputTemplateBackupHasSchedule/Save', token, body,'c70001a1-4d36-a4b4-0c68-117c7359e164',1);
     });
-
+    //#endregion
 
 })
--- cypress/support/request.js
+++ cypress/support/request.js
@@ -1,4 +1,4 @@
-Cypress.Commands.add('getRequest', (url,token,itemsResponse,fileName) => {
+Cypress.Commands.add('getRequest', (url, token, itemsResponse, fileName) => {
     cy.request({
         method: "GET",
         url: url,
@@ -21,12 +21,46 @@ Cypress.Commands.add('getRequest', (url,token,itemsResponse,fileName) => {
             "sec-fetch-mode": "cors",
             "sec-fetch-site": "same-origin",
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36",
-    
+
         }
     }).then(function (response) {
         console.log(response)
         expect(response.body).have.length(itemsResponse);
         cy.writeFile(fileName, response.body)
     });
-    
+
+})
+
+Cypress.Commands.add('postRequest', (url, token, body, valueResponse, option) => {
+    cy.request({
+        method: "POST",
+        url: url,
+        body: body,
+        headers: {
+            "accept": "application/json, text/plain",
+            "accept-encoding": "gzip, deflate, br",
+            "accept-language": "es,it-IT;q=0.9,it;q=0.8,en-US;q=0.7,en;q=0.6",
+            "appversion": "1.58.0.22",
+            "Authorization": token,
+            "clienttabid": "7519A63F-9D18-F8FE-CC40-ECFB920AAFBD",
+            "cnet.cumap3.web.clientagentid": "99188DB9-4AEB-6FCC-28B2-D0D305A1A503",
+            "cnet.cumap3.web.clientapplicationid": "10001",
+            "cookie": "CNET.Cumap3.Web.ClientAgentCookie=C56A57FB-B3A7-0F8D-17A7-498A5F45257E; ai_user=pgEMG|2022-02-22T13:25:15.071Z",
+            "operationrequestid": "1C6645E8-4343-EB61-98A6-2992707F8DB0",
+            "referer": "https://debugsite.myequipe2/app/",
+            "sec-ch-ua": ' " Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "Windows",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36",
+        },
+    }).then(function (response) {
+        if (option == 1) {
+            expect(response.body.careFolderOutputTemplateBackupId).eq(valueResponse)
+        } else {
+            expect(response.body.id).eq(valueResponse)
+        }
+    });
 })
